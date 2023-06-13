02df5dbf89c41eb4a2fbab2644afbdec374ca80e
Siddu0201 <sudhanshuag02012000@gmail.com>
2023-06-13 12:55:23 +0530

    remove build from index

--- build/front.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'deec45af8260d9d1a497');
--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'a97856dd50cae6edef64');
