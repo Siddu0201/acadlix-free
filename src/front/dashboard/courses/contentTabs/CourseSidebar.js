7c9c0fc346ec64352688bedd1909ac9f941d835b
Siddu0201 <sudhanshuag02012000@gmail.com>
2024-06-01 13:25:36 +0530

    Update Sidebarlist

--- src/front/dashboard/courses/contentTabs/CourseSidebar.js
+++ src/front/dashboard/courses/contentTabs/CourseSidebar.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import {
   Box,
 } from "@mui/material";
-import SidebarList from "../CourseComponents/SidebarList";
+import SidebarList from "../courseComponents/SidebarList";
 
 const CourseSidebar = () => {
   
