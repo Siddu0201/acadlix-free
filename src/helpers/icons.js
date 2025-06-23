fe69218a99e97b1e7ff920785850c195e6404fdc
Siddu0201 <sudhanshuag02012000@gmail.com>
2025-06-23 13:09:19 +0530

    add icon and update acadlix plugin url

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -102,7 +102,7 @@ export const TiArrowLeftThick = (props) => {
 
 export const FaParagraph = (props) => {
     return (
-        <svg {...props} stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M448 48v32a16 16 0 0 1-16 16h-48v368a16 16 0 0 1-16 16h-32a16 16 0 0 1-16-16V96h-32v368a16 16 0 0 1-16 16h-32a16 16 0 0 1-16-16V352h-32a160 160 0 0 1 0-320h240a16 16 0 0 1 16 16z"></path></svg> 
+        <svg {...props} stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M448 48v32a16 16 0 0 1-16 16h-48v368a16 16 0 0 1-16 16h-32a16 16 0 0 1-16-16V96h-32v368a16 16 0 0 1-16 16h-32a16 16 0 0 1-16-16V352h-32a160 160 0 0 1 0-320h240a16 16 0 0 1 16 16z"></path></svg>
     )
 }
 
@@ -410,21 +410,27 @@ export const EvaluationIcon = (props) => {
                 <title></title>
                 <desc></desc>
                 <g id="Stroke_copy_3">
-                    <path d="M42,47A24,24,0,0,1,18,23V7H11V51a4,4,0,0,0,4,4H51V47Z" style={{fill:"#cff09e"}}></path>
-                    <path d="M15,3H51a0,0,0,0,1,0,0v8a0,0,0,0,1,0,0H15a4,4,0,0,1-4-4V7a4,4,0,0,1,4-4Z" style={{fill:"#a8dba8"}}></path>
-                    <rect height="6" style={{fill:"#79bd9a"}} width="22" x="21" y="19"></rect>
-                    <circle cx="47" cy="47" r="14" style={{fill:"#a8dba8"}}></circle>
+                    <path d="M42,47A24,24,0,0,1,18,23V7H11V51a4,4,0,0,0,4,4H51V47Z" style={{ fill: "#cff09e" }}></path>
+                    <path d="M15,3H51a0,0,0,0,1,0,0v8a0,0,0,0,1,0,0H15a4,4,0,0,1-4-4V7a4,4,0,0,1,4-4Z" style={{ fill: "#a8dba8" }}></path>
+                    <rect height="6" style={{ fill: "#79bd9a" }} width="22" x="21" y="19"></rect>
+                    <circle cx="47" cy="47" r="14" style={{ fill: "#a8dba8" }}></circle>
                 </g>
                 <g id="Stroke_Close_copy_3">
-                    <path d="M17,8H27a1,1,0,0,0,0-2H17a1,1,0,0,0,0,2Z" style={{fill:"#3b8686"}}></path>
-                    <path d="M52,32.88V3a1,1,0,0,0-1-1H15a5,5,0,0,0-5,5V51a5,5,0,0,0,5,5H35A15,15,0,1,0,52,32.88ZM15,4H50V6H31a1,1,0,0,0,0,2H50v2H15a3,3,0,0,1,0-6Zm0,50a3,3,0,0,1-3-3V11a4.92,4.92,0,0,0,3,1H50V32.3a14.82,14.82,0,0,0-3-.3A15,15,0,0,0,33.74,54Zm32,6A13,13,0,1,1,60,47,13,13,0,0,1,47,60Z" style={{fill:"#3b8686"}}></path>
-                    <path d="M53.85,41.22,43.24,51.83l-3.53-3.54a1,1,0,0,0-1.42,1.42L42.54,54a1,1,0,0,0,.7.29A1,1,0,0,0,44,54L55.26,42.64a1,1,0,0,0,0-1.42A1,1,0,0,0,53.85,41.22Z" style={{fill:"#3b8686"}}></path>
-                    <path d="M21,26H43a1,1,0,0,0,1-1V19a1,1,0,0,0-1-1H21a1,1,0,0,0-1,1v6A1,1,0,0,0,21,26Zm1-6H42v4H22Z" style={{fill:"#3b8686"}}></path>
-                    <path d="M21,32H37a1,1,0,0,0,0-2H21a1,1,0,0,0,0,2Z" style={{fill:"#3b8686"}}></path>
-                    <path d="M31,36H21a1,1,0,0,0,0,2H31a1,1,0,0,0,0-2Z" style={{fill:"#3b8686"}}></path>
-                    <path d="M27,42H21a1,1,0,0,0,0,2h6a1,1,0,0,0,0-2Z" style={{fill:"#3b8686"}}></path>
+                    <path d="M17,8H27a1,1,0,0,0,0-2H17a1,1,0,0,0,0,2Z" style={{ fill: "#3b8686" }}></path>
+                    <path d="M52,32.88V3a1,1,0,0,0-1-1H15a5,5,0,0,0-5,5V51a5,5,0,0,0,5,5H35A15,15,0,1,0,52,32.88ZM15,4H50V6H31a1,1,0,0,0,0,2H50v2H15a3,3,0,0,1,0-6Zm0,50a3,3,0,0,1-3-3V11a4.92,4.92,0,0,0,3,1H50V32.3a14.82,14.82,0,0,0-3-.3A15,15,0,0,0,33.74,54Zm32,6A13,13,0,1,1,60,47,13,13,0,0,1,47,60Z" style={{ fill: "#3b8686" }}></path>
+                    <path d="M53.85,41.22,43.24,51.83l-3.53-3.54a1,1,0,0,0-1.42,1.42L42.54,54a1,1,0,0,0,.7.29A1,1,0,0,0,44,54L55.26,42.64a1,1,0,0,0,0-1.42A1,1,0,0,0,53.85,41.22Z" style={{ fill: "#3b8686" }}></path>
+                    <path d="M21,26H43a1,1,0,0,0,1-1V19a1,1,0,0,0-1-1H21a1,1,0,0,0-1,1v6A1,1,0,0,0,21,26Zm1-6H42v4H22Z" style={{ fill: "#3b8686" }}></path>
+                    <path d="M21,32H37a1,1,0,0,0,0-2H21a1,1,0,0,0,0,2Z" style={{ fill: "#3b8686" }}></path>
+                    <path d="M31,36H21a1,1,0,0,0,0,2H31a1,1,0,0,0,0-2Z" style={{ fill: "#3b8686" }}></path>
+                    <path d="M27,42H21a1,1,0,0,0,0,2h6a1,1,0,0,0,0-2Z" style={{ fill: "#3b8686" }}></path>
                 </g>
             </g>
         </svg>
+    )
+}
+
+export const PiCalculator = (props) => {
+    return (
+        <svg {...props} stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 256 256" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M80,120h96a8,8,0,0,0,8-8V64a8,8,0,0,0-8-8H80a8,8,0,0,0-8,8v48A8,8,0,0,0,80,120Zm8-48h80v32H88ZM200,24H56A16,16,0,0,0,40,40V216a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V40A16,16,0,0,0,200,24Zm0,192H56V40H200ZM100,148a12,12,0,1,1-12-12A12,12,0,0,1,100,148Zm40,0a12,12,0,1,1-12-12A12,12,0,0,1,140,148Zm40,0a12,12,0,1,1-12-12A12,12,0,0,1,180,148Zm-80,40a12,12,0,1,1-12-12A12,12,0,0,1,100,188Zm40,0a12,12,0,1,1-12-12A12,12,0,0,1,140,188Zm40,0a12,12,0,1,1-12-12A12,12,0,0,1,180,188Z"></path></svg>
     )
 }
