72f3c2e0c493fc8c14701266a4d76a5d62812343
Siddu0201 <sudhanshuag02012000@gmail.com>
2025-06-23 12:28:56 +0530

    Update QuizContent.js

--- src/front/dashboard/quiz/QuizContent.js
+++ src/front/dashboard/quiz/QuizContent.js
@@ -92,6 +92,7 @@ const QuizContent = (props) => {
       specific_number_of_questions: props?.quiz?.rendered_metas?.quiz_settings?.specific_number_of_questions, // 0 => all
       result_feedback_by_ai: Boolean(Number(props?.quiz?.rendered_metas?.quiz_settings?.result_feedback_by_ai)),
       result_feedback_additional_prompt: props?.quiz?.rendered_metas?.quiz_settings?.result_feedback_additional_prompt ?? "",
+      scientific_calculator: Boolean(Number(props?.quiz?.rendered_metas?.quiz_settings?.scientific_calculator)),
       // Question settings
       show_marks: Boolean(Number(props?.quiz?.rendered_metas?.quiz_settings?.show_marks)),
       display_subject: Boolean(Number(props?.quiz?.rendered_metas?.quiz_settings?.display_subject)),
