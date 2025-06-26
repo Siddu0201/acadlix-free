b0c39e921c4e3f197fba39f4edfa9337853e7af4
Siddu0201 <sudhanshuag02012000@gmail.com>
2025-06-26 12:36:09 +0530

    test for dynamic versioning

--- acadlix.php
+++ acadlix.php
@@ -5,7 +5,7 @@
  * Description:       Wordpress advance quiz plugin by yuvayana
  * Requires at least: 6.8
  * Requires PHP:      8.0
- * Version:           1.0.1
+ * Version:           {{VERSION}}
  * Tested upto:       6.8.1
  * Author:            Team Acadlix
  * Author URI:        https://acadlix.com/
@@ -22,14 +22,20 @@ if (!defined('ACADLIX_PLUGIN_FILE')) {
     include_once 'includes/acadlix-contant.php';
 }
 
-if(!defined('ACADLIX_VERSION')){
-    define('ACADLIX_VERSION', '1.0.1');
+if (!defined('ACADLIX_VERSION')) {
+    // define('ACADLIX_VERSION', '1.0.1');
+    if (!function_exists('get_file_data')) {
+        require_once ABSPATH . 'wp-includes/functions.php';
+    }
+
+    $plugin_data = get_file_data(__FILE__, ['Version' => 'Version']);
+    define('ACADLIX_VERSION', $plugin_data['Version']);
 }
 
-if(function_exists('acadlix')){
+if (function_exists('acadlix')) {
     return;
 }
 
-require_once ACADLIX_INCLUDES_PATH .'/AcadlixAbstract.php';
-require_once ACADLIX_INCLUDES_PATH .'/Acadlix.php';
+require_once ACADLIX_INCLUDES_PATH . '/AcadlixAbstract.php';
+require_once ACADLIX_INCLUDES_PATH . '/Acadlix.php';
 acadlix();
