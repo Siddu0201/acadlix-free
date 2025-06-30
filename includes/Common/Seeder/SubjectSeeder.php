13c1dd76a3dfa7eb026c53c8f1d32aa55f0fabbb
Siddu0201 <sudhanshuag02012000@gmail.com>
2025-06-30 15:00:46 +0530

    Update SubjectSeeder.php

--- includes/Common/Seeder/SubjectSeeder.php
+++ includes/Common/Seeder/SubjectSeeder.php
@@ -11,8 +11,9 @@ if(!class_exists('SubjectSeeder')){
         {
             $subject_name = "Uncategorized";
             $subject = acadlix()->model()->subject()->get();
-            if($subject->count() == 0){
-                $subject = acadlix()->model()->subject()->create([
+            $default_subject = acadlix()->model()->subject()->where('default', true)->get();
+            if($subject->count() == 0 || $default_subject->count() == 0){
+                acadlix()->model()->subject()->create([
                     "subject_name" => $subject_name,
                     "default" => true
                 ]);
