971ee6c964b6e5a24c160622c6ad991b00c0e84e
Siddu0201 <sudhanshuag02012000@gmail.com>
2025-07-12 17:54:29 +0530

    Update front-checkout.js

--- src/front-checkout.js
+++ src/front-checkout.js
@@ -1,13 +1,14 @@
 import { jsx as _jsx } from 'react/jsx-runtime'
 import { createRoot } from 'react-dom/client';
 import FrontCheckout from "@acadlix/front/FrontCheckout";
+import domReady from '@wordpress/dom-ready';
 import { loadFrontCheckoutHooks } from '@acadlix/modules/extensions/hooksLoader';
 
-(async () => {
-    await loadFrontCheckoutHooks(window?.acadlixHooks);
+await loadFrontCheckoutHooks(window?.acadlixHooks);
+domReady(() => {
     const frontCheckout = document.getElementById("acadlix_checkout");
-    if(frontCheckout){
+    if (frontCheckout) {
         const frontCheckoutRoot = createRoot(frontCheckout);
         frontCheckoutRoot.render(<FrontCheckout></FrontCheckout>);
     }
-})
+});
