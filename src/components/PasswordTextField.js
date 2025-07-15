fca4dd91c51916ec0c7497d41875e1c7e8c48600
Siddu0201 <sudhanshuag02012000@gmail.com>
2025-07-15 14:00:18 +0530

    Update PasswordTextField.js

--- src/components/PasswordTextField.js
+++ src/components/PasswordTextField.js
@@ -3,21 +3,12 @@ import { IconButton, InputAdornment } from '@mui/material';
 import React from 'react'
 import CustomTextField from './CustomTextField';
 
-const PasswordTextField = ({ 
-    label = "",
-    name = "",
-    onChange = () => {},
-    formProps = {},
-}) => {
+const PasswordTextField = (props) => {
     const [visible, setVisible] = React.useState(false);
     return (
         <CustomTextField
-            fullWidth
-            size="small"
+            {...props}
             type={visible ? "text" : "password"}
-            label={label}
-            value={formProps?.watch(name)}
-            onChange={onChange}
             InputProps={{
                 endAdornment: (
                     <InputAdornment position="end">
