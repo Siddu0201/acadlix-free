427408f782650558cfa291fdae4d4ec89ef151f8
Siddu0201 <sudhanshuag02012000@gmail.com>
2025-09-12 11:43:35 +0530

    Update LiteSpeedCache.php

--- includes/Common/Integrations/Caches/LiteSpeedCache.php
+++ includes/Common/Integrations/Caches/LiteSpeedCache.php
@@ -10,6 +10,7 @@ if (!class_exists('LiteSpeedCache')) {
         public function __construct()
         {
             add_filter('litespeed_optimize_js_excludes', [$this, 'js_exclude']);
+            add_filter('litespeed_optimize_css_excludes ', [$this, 'css_exclude']);
         }
 
         public function js_exclude($excluded)
@@ -18,6 +19,15 @@ if (!class_exists('LiteSpeedCache')) {
                 $excluded = [];
             }
             $excluded[] = 'wp';
+            $excluded[] = 'acadlix';
+            return $excluded;
+        }
+
+        public function css_exclude($excluded)
+        {
+            if(!is_array($excluded)) {
+                $excluded = [];
+            }
             $excluded[] = 'acadlix';
             return $excluded;
         }
