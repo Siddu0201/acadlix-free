678fd8bf1138339be4bf03b2b0dad6f0720b7a34
Siddu0201 <sudhanshuag02012000@gmail.com>
2025-10-18 15:34:55 +0530

    Update DashboardNavbar.js

--- src/partials/DashboardNavbar.js
+++ src/partials/DashboardNavbar.js
@@ -1,10 +1,78 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import dashboardMenu from "@acadlix/menu/dashboardMenu";
 import { __ } from "@wordpress/i18n";
 import { DynamicMUIRenderer } from "@acadlix/modules/extensions/muiRecursiveRenderer";
 
 const DashboardNavbar = ({ handleDrawerToggle, isDesktop }) => {
+  const dashboardMenu = window.acadlixHooks?.applyFilters(
+    'acadlix.front.dashboard.menu',
+    [
+      {
+        id: "home",
+        name: __("Home", 'acadlix'),
+        path: acadlixOptions?.home_url,
+        isRedirect: true,
+        icon: "FaHome",
+        default: true,
+        enabled: acadlixOptions?.settings?.acadlix_disable_home_menu === 'no',
+      },
+      {
+        id: "courses",
+        name: __("My Courses", 'acadlix'),
+        path: "/courses",
+        isRedirect: false,
+        icon: "MdLibraryBooks",
+        default: true,
+        enabled: true,
+      },
+      {
+        id: "result",
+        name: __("My Result", 'acadlix'),
+        path: "/result",
+        isRedirect: false,
+        icon: "MdAssessment",
+        default: true,
+        enabled: true,
+      },
+      {
+        id: "purcahse",
+        name: __("Purchase History", 'acadlix'),
+        path: "/purchase",
+        isRedirect: false,
+        icon: "MdReceiptLong",
+        default: true,
+        enabled: true,
+      },
+      {
+        id: "wishlist",
+        name: __("Wishlist", 'acadlix'),
+        path: "/wishlist",
+        isRedirect: false,
+        icon: "FaHeart",
+        default: true,
+        enabled: acadlixOptions?.settings?.acadlix_disable_wishlist === 'no',
+      },
+      {
+        id: "profile",
+        name: __("Profile", 'acadlix'),
+        path: "/profile",
+        isRedirect: false,
+        icon: "FaUserCircle",
+        default: true,
+        enabled: true,
+      },
+      {
+        id: "logout",
+        name: __("Logout", 'acadlix'),
+        path: acadlixOptions?.logout_url,
+        isRedirect: true,
+        icon: "FiLogOut",
+        default: true,
+        enabled: true,
+      },
+    ],
+    { acadlixOptions })?.filter(Boolean) || [];
+
   const defaultSetting = {
     component: "List",
     component_name: "dashboard_navbar_list",
@@ -26,7 +94,7 @@ const DashboardNavbar = ({ handleDrawerToggle, isDesktop }) => {
         <React.Fragment key={i}>
           <DynamicMUIRenderer
             item={field}
-            index={i}
+            key={i}
             formProps={{}}
           />
         </React.Fragment>
@@ -108,7 +176,7 @@ const DashboardMenuItem = ({ route, index, handleDrawerToggle, isDesktop }) => {
         <React.Fragment key={i}>
           <DynamicMUIRenderer
             item={field}
-            index={i}
+            key={i}
             formProps={{}}
           />
         </React.Fragment>
