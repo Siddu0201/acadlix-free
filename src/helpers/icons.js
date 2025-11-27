e4646ff414fa42460e5f5116d7929d059bda32aa
Siddu0201 <sudhanshuag02012000@gmail.com>
2025-11-27 18:20:53 +0530

    add social icon

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -648,6 +648,31 @@ export const SocialLogin = (props) => {
     )
 }
 
+export const GoogleIcon = (props) => {
+    return (
+        <svg {...props} width="18" height="18" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" aria-hidden>
+            <path fill="#EA4335" d="M24 9.5c3.9 0 7.1 1.3 9.4 3.1l7-6.8C36.5 2.8 30.7 0 24 0 14.7 0 6.9 4.9 2.9 12l8.5 6.6C13.8 14.1 18.5 9.5 24 9.5z" />
+            <path fill="#4285F4" d="M46.5 24.5c0-1.6-.1-3.1-.4-4.6H24v9.1h12.6c-.5 3-2.6 5.5-5.6 7.1l8.6 6.7C44.9 38.2 46.5 31.9 46.5 24.5z" />
+            <path fill="#FBBC05" d="M10.6 28.6A14.7 14.7 0 0 1 9.5 24c0-1.6.3-3.1.8-4.6L2.9 12C1 15.9 0 20.8 0 24.5c0 3.7 1 8.6 2.9 12l7.7-7.9z" />
+            <path fill="#34A853" d="M24 48c6.7 0 12.5-2.2 16.7-6l-8.6-6.7C31.1 36.5 27.9 37.8 24 37.8 18.5 37.8 13.8 33.2 11.4 28.6l-8.5 6.6C6.9 43.1 14.7 48 24 48z" />
+        </svg>
+    )
+}
+
+export const FacebookIcon = (props) => {
+    return (
+        <svg
+            {...props}
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+        >
+            <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
+        </svg>
+    )
+}
+
 export const iconMap = {
     IoClose: <IoClose />,
     IoMdRefresh: <IoMdRefresh />,
@@ -749,5 +774,7 @@ export const iconMap = {
     FaFilePdf: <FaFilePdf />,
     FaFileExport: <FaFileExport />,
     TbAlertTriangleFilled: <TbAlertTriangleFilled />,
-    SocialLogin: <SocialLogin />
+    SocialLogin: <SocialLogin />,
+    GoogleIcon: <GoogleIcon />,
+    FacebookIcon: <FacebookIcon />
 };
