f320424101b5aac94181bbe94ead8952a8c6694d
Siddu0201 <sudhanshuag02012000@gmail.com>
2026-01-17 11:05:00 +0530

    convert front dashboard result to jsx

--- src/front/dashboard/result/Result.js
+++ src/front/dashboard/result/Result.js
@@ -23,6 +23,8 @@ import dateFormat from "dateformat";
 import { GetStatisticByUserId } from "@acadlix/requests/front/FrontStatisticRequest";
 import { useNavigate } from "react-router-dom";
 import CustomRefresh from "@acadlix/components/CustomRefresh";
+import { DynamicMUIRenderer } from "@acadlix/modules/extensions/muiRecursiveRenderer";
+import Loader from "@acadlix/components/Loader";
 
 export default function Result() {
   const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
@@ -195,7 +197,14 @@ export default function Result() {
                   flexDirection: 'column',
                 }}
               >
-                {
+                <MobileOnlyView
+                  {...methods}
+                  isFetching={isFetching}
+                  paginationModel={paginationModel}
+                  handlePaginationChange={handlePaginationChange}
+                  rowCount={rowCount}
+                />
+                {/* {
                   isMobile ? (
                     <MobileOnlyView
                       {...methods}
@@ -235,7 +244,7 @@ export default function Result() {
                       }}
                     />
                   )
-                }
+                } */}
               </Box>
             </CardContent>
           </Card>
@@ -246,122 +255,239 @@ export default function Result() {
 }
 
 const MobileOnlyView = (props) => {
+  const defaultSetting = {
+    component: "Paper",
+    component_name: "result_paper",
+    props: {
+      sx: {
+        width: "100%",
+        backgroundColor: "transparent",
+        border: "none",
+        boxShadow: "none",
+      }
+    },
+    children: [
+      {
+        component: "Fragment",
+        component_name: "result_fragment",
+        children: props?.watch("rows")?.length > 0 ?
+          props?.watch("rows")?.map((row, index) => {
+            return {
+              component: <SingleResult row={row} key={index} {...props} />,
+              component_name: "result_content_item"
+            }
+          })
+          : [{
+            component: "Box",
+            component_name: "result_box_no_data",
+            props: {
+              sx: {
+                display: "flex",
+                justifyContent: "center",
+                padding: 2,
+              }
+            },
+            children: [
+              {
+                component_name: "result_box_no_data_text",
+                component: "Typography",
+                props: {
+                  variant: "h6",
+                  sx: {
+                    color: "text.secondary",
+                  }
+                },
+                value: __("No Data Found", "acadlix")
+              }
+            ]
+          }],
+      },
+      {
+        component: "Box",
+        props: {
+          sx: {
+            display: "flex",
+            justifyContent: "center",
+            padding: 1,
+          }
+        },
+        children: [
+          {
+            component: "TablePagination",
+            component_name: "result_table_pagination",
+            props: {
+              component: "div",
+              count: props?.rowCount,
+              page: props?.paginationModel?.page,
+              onPageChange: (_, newPage) => props?.handlePaginationChange({ ...props?.paginationModel, page: newPage }),
+              rowsPerPage: props?.paginationModel?.pageSize,
+              onRowsPerPageChange: (e) => {
+                const pageSize = parseInt(e?.target?.value);
+                const page = Math.min(props?.paginationModel?.page, Math.floor(props?.watch("rows").length / pageSize)); // Ensure page does not exceed limit
+                props?.handlePaginationChange({
+                  pageSize: pageSize,
+                  page: page,
+                })
+              },
+              sx: {
+                '& .MuiToolbar-root': {
+                  paddingLeft: 0,
+                  paddingRight: 0,
+                },
+                '& .MuiTablePagination-selectLabel': {
+                  margin: 0,
+                },
+                '& .MuiTablePagination-displayedRows': {
+                  margin: 0,
+                },
+                '& .MuiInputBase-root': {
+                  marginX: 0,
+                },
+              },
+            }
+          }
+        ]
+      }
+    ]
+  }
+
+  const resultMobile = window.acadlixHooks?.applyFilters(
+    'acadlix.front.dashboard.result.mobile',
+    [defaultSetting],
+    {
+      register: props?.register,
+      watch: props?.watch,
+      setValue: props?.setValue,
+      handlePaginationChange: props?.handlePaginationChange,
+      paginationModel: props?.paginationModel,
+      isDesktop: false,
+    }
+  )?.filter(Boolean) || [];
+
+  if (props?.isFetching) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      {resultMobile.map((field, i) => (
+        <React.Fragment key={i}>
+          <DynamicMUIRenderer
+            item={field}
+            key={i}
+            formProps={{
+              register: props?.register,
+              watch: props?.watch,
+              setValue: props?.setValue,
+            }}
+          />
+        </React.Fragment>
+      ))}
+    </>
+  )
+
   return (
     <Paper style={{
       width: "100%",
       backgroundColor: "transparent",
       border: "none",
       boxShadow: "none"
-    }}>
-      {/* <Box padding={1} sx={{ mx: 1 }}>
-        <TextField
-          fullWidth
-          variant="outlined"
-          placeholder={__("Search Courses", "acadlix")}
-          sx={{
-            "& .MuiInputBase-root": {
-              height: "32px",
-              fontSize: "12px",
-              width: "99%",
-            },
-          }}
-        />
-      </Box> */}
-
-      {props?.isFetching ? (
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            padding: 2,
-          }}
-        >
-          <CircularProgress />
-        </Box>
-      ) : (
-        props?.watch("rows")?.map((row, index) => {
-          let color = "default";
-          let label = "";
-          if (row?.status === "Pass") {
-            color = "success";
-            label = __("Pass", "acadlix");
-          } else if (row?.status === "Fail") {
-            color = "error";
-            label = __("Fail", "acadlix");
-          } else if (row?.status === "NA") {
-            color = "grey";
-            label = __("NA", "acadlix");
-          }
-          return (
-            <Box
-              key={index}
-              sx={{
-                padding: "8px",
-                marginTop: "8px",
-                marginBottom: "8px",
-                borderBottom: "1px solid #e0e0e0",
-                display: "flex",
-                flexDirection: "column",
-                gap: "4px",
-                boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.2)",
-                borderRadius: "8px",
-                backgroundColor: "white",
-              }}
-            >
+    }} >
+      {
+        props?.isFetching ? (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              padding: 2,
+            }}
+          >
+            <CircularProgress />
+          </Box >
+        ) : (
+          props?.watch("rows")?.map((row, index) => {
+            let color = "default";
+            let label = "";
+            if (row?.status === "Pass") {
+              color = "success";
+              label = __("Pass", "acadlix");
+            } else if (row?.status === "Fail") {
+              color = "error";
+              label = __("Fail", "acadlix");
+            } else if (row?.status === "NA") {
+              color = "grey";
+              label = __("NA", "acadlix");
+            }
+            return (
               <Box
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
+                key={index}
+                sx={{
+                  padding: "8px",
+                  marginTop: "8px",
+                  marginBottom: "8px",
+                  borderBottom: "1px solid #e0e0e0",
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "4px",
+                  boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.2)",
+                  borderRadius: "8px",
+                  backgroundColor: "white",
+                }}
               >
-                <Typography
-                  variant="h6"
-                  sx={{
-                    fontSize: "14px",
-                  }}
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
                 >
-                  {row?.title}
-                </Typography>
-                <Tooltip title={__("View Answersheet", "acadlix")} arrow>
-                  <IconButton
-                    aria-label="expand"
-                    size="small"
-                    color="warning"
-                    LinkComponent={Link}
-                    to={`/result/${row?.id}`}
-                    disabled={row?.hide_answer_sheet}
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      fontSize: "14px",
+                    }}
                   >
-                    <FaExpandArrowsAlt fontSize="inherit" />
-                  </IconButton>
-                </Tooltip>
-              </Box>
-              <Box display="flex" alignItems="center" gap="4px">
-                <HistoryToggleOff style={{ color: "gray" }} />
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
+                    {row?.title}
+                  </Typography>
+                  <Tooltip title={__("View Answersheet", "acadlix")} arrow>
+                    <IconButton
+                      aria-label="expand"
+                      size="small"
+                      color="warning"
+                      LinkComponent={Link}
+                      to={`/result/${row?.id}`}
+                      disabled={row?.hide_answer_sheet}
+                    >
+                      <FaExpandArrowsAlt fontSize="inherit" />
+                    </IconButton>
+                  </Tooltip>
+                </Box>
+                <Box display="flex" alignItems="center" gap="4px">
+                  <HistoryToggleOff style={{ color: "gray" }} />
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {row?.date}
+                  </Typography>
+                </Box>
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
                 >
-                  {row?.date}
-                </Typography>
+                  <Typography
+                    variant="body2"
+                  >
+                    {__("Score: ", "acadlix")}{row?.score}
+                  </Typography>
+                  <Chip
+                    color={color}
+                    label={label}
+                  />
+                </Box>
               </Box>
-              <Box
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-              >
-                <Typography
-                  variant="body2"
-                >
-                  {__("Score: ", "acadlix")}{row?.score}
-                </Typography>
-                <Chip
-                  color={color}
-                  label={label}
-                />
-              </Box>
-            </Box>
-          )
-        })
-      )}
+            )
+          })
+        )}
 
       <Box display="flex" justifyContent="center" padding={1}>
         <TablePagination
@@ -407,6 +533,176 @@ const MobileOnlyView = (props) => {
           /> */}
         {/* </Stack> */}
       </Box>
-    </Paper>
+    </Paper >
   );
 };
+
+const SingleResult = ({ row, ...props }) => {
+  let color = "default";
+  let label = "";
+  if (row?.status === "Pass") {
+    color = "success";
+    label = __("Pass", "acadlix");
+  } else if (row?.status === "Fail") {
+    color = "error";
+    label = __("Fail", "acadlix");
+  } else if (row?.status === "NA") {
+    color = "grey";
+    label = __("NA", "acadlix");
+  }
+
+  const defaultSetting = {
+    component: "Box",
+    component_name: "single_result_box",
+    props: {
+      sx: {
+        padding: "8px",
+        marginTop: "8px",
+        marginBottom: "8px",
+        borderBottom: "1px solid #e0e0e0",
+        display: "flex",
+        flexDirection: "column",
+        gap: "4px",
+        boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.2)",
+        borderRadius: "8px",
+        backgroundColor: "white",
+      },
+    },
+    children: [
+      {
+        component: "Box",
+        component_name: "single_result_box_header",
+        props: {
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        },
+        children: [
+          {
+            component: "Typography",
+            component_name: "single_result_box_header_typography",
+            props: {
+              variant: "h6",
+              sx: {
+                fontSize: "14px",
+              },
+            },
+            value: row?.title,
+          },
+          {
+            component: "Tooltip",
+            component_name: "single_result_box_header_tooltip",
+            props: {
+              title: __("View Answersheet", "acadlix"),
+              arrow: true,
+            },
+            children: [
+              {
+                component: "IconButton",
+                component_name: "single_result_box_header_tooltip_iconbutton",
+                props: {
+                  size: "small",
+                  color: "warning",
+                  LinkComponent: Link,
+                  to: `/result/${row?.id}`,
+                  disabled: row?.hide_answer_sheet,
+                },
+                children: [
+                  {
+                    component: "FaExpandArrowsAlt",
+                    component_name: "single_result_box_header_tooltip_iconbutton_icon",
+                    props: {
+                      fontSize: "inherit",
+                    },
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        component: "Box",
+        component_name: "single_result_box_date",
+        props: {
+          display: "flex",
+          alignItems: "center",
+          gap: "4px",
+        },
+        children: [
+          {
+            component: "HistoryToggleOff",
+            component_name: "single_result_box_date_icon",
+            props: {
+              style: { color: "gray" },
+            },
+          },
+          {
+            component: "Typography",
+            component_name: "single_result_box_date_typography",
+            props: {
+              variant: "body2",
+              color: "text.secondary",
+            },
+            value: row?.date,
+          }
+        ]
+      },
+      {
+        component: "Box",
+        component_name: "single_result_box_score",
+        props: {
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        },
+        children: [
+          {
+            component: "Typography",
+            component_name: "single_result_box_score_typography",
+            props: {
+              variant: "body2",
+            },
+            value: __("Score: ", "acadlix") + row?.score,
+          },
+          {
+            component: "Chip",
+            component_name: "single_result_box_score_chip",
+            props: {
+              color: color,
+              label: label,
+            },
+          }
+        ]
+      }
+    ],
+  }
+
+  const singleResult = window.acadlixHooks?.applyFilters(
+    'acadlix.front.dashboard.result.singleResult',
+    [defaultSetting],
+    {
+      register: props?.register,
+      watch: props?.watch,
+      setValue: props?.setValue,
+      isDesktop: props?.isDesktop,
+      row: row,
+    }
+  )?.filter(Boolean) || [];
+
+  return (
+    <>
+      {singleResult.map((field, i) => (
+        <DynamicMUIRenderer
+          item={field}
+          key={i}
+          formProps={{
+            register: props?.register,
+            watch: props?.watch,
+            setValue: props?.setValue,
+          }}
+        />
+      ))}
+    </>
+  )
+}
