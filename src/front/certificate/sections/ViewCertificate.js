b6b3f17af492f4055501edb18b69779e070e5ab3
Siddu0201 <sudhanshuag02012000@gmail.com>
2026-04-16 11:10:56 +0530

    resolve react pdf issue

--- src/front/certificate/sections/ViewCertificate.js
+++ src/front/certificate/sections/ViewCertificate.js
@@ -1,18 +1,18 @@
-// import TemplateRenderer from '@acadlix/templates/TemplateRenderer'
-// import { PDFViewer } from '@react-pdf/renderer'
+import TemplateRenderer from '@acadlix/templates/TemplateRenderer'
+import { PDFViewer } from '@react-pdf/renderer'
 import React from 'react'
 
 const ViewCertificate = (props) => {
   return (
     <>
-      {/* <PDFViewer width="100%" height="100%">
-      <TemplateRenderer
-        type="certificates"
-        subtype="course"
-        template={props.watch('template')}
-        {...props}
-      />
-    </PDFViewer> */}
+      <PDFViewer width="100%" height="100%">
+        <TemplateRenderer
+          type="certificates"
+          subtype="course"
+          template={props.watch('template')}
+          {...props}
+        />
+      </PDFViewer>
     </>
   )
 }
