bfd700d85740c2fcf125038cfac034024c6cc02b
Siddu0201 <sudhanshuag02012000@gmail.com>
2026-05-13 18:38:07 +0530

    update font

--- src/pdf/fontHelper.js
+++ src/pdf/fontHelper.js
@@ -1,12 +1,8 @@
-// Detect Devanagari characters
-export const isHindi = (text = "") => {
-  return /[\u0900-\u097F]/.test(text);
-};
-
 export const getFontFamily = (text) => {
-  // if (isHindi(text)) {
-  //   return "NotoSansDevanagari";
-  // }
+  if (/[\u0900-\u097F]/.test(text)) return "NotoDevanagari"; // Hindi
+  if (/[\u0E00-\u0E7F]/.test(text)) return "NotoThai";       // Thai
+  if (/[\u0600-\u06FF]/.test(text)) return "NotoArabic";     // Arabic
+  if (/[\u3040-\u30FF\u4E00-\u9FFF]/.test(text)) return "NotoJP"; // Japanese
 
-  return "AcadlixUniversal";
+  return "NotoLatin"; // default
 };
--- src/pdf/registerFont.js
+++ src/pdf/registerFont.js
@@ -1,40 +1,26 @@
 import { Font } from "@react-pdf/renderer";
 
 Font.register({
-  family: "AcadlixUniversal",
-  fonts: [
-    {
-      src: `${acadlixOptions?.pdf_url}fonts/NotoSans-Regular.ttf`,
-      fontWeight: "normal",
-    },
-    {
-      src: `${acadlixOptions?.pdf_url}fonts/NotoSans-Bold.ttf`,
-      fontWeight: "bold",
-    },
-    {
-      src: `${acadlixOptions?.pdf_url}fonts/NotoSansDevanagari-Regular.ttf`,
-    },
-    {
-      src: `${acadlixOptions?.pdf_url}fonts/NotoSansThai-Regular.ttf`,
-    },
-    {
-      src: `${acadlixOptions?.pdf_url}fonts/NotoSansArabic-Regular.ttf`,
-    },
-    {
-      src: `${acadlixOptions?.pdf_url}fonts/NotoSansJP-Regular.ttf`,
-    },
-  ],
+  family: "NotoLatin",
+  src: `${acadlixOptions?.pdf_url}fonts/NotoSans-Regular.ttf`,
 });
 
-// Font.register({
-//   family: "NotoSansDevanagari",
-//   fonts: [
-//     {
-//       fontWeight: "normal",
-//     },
-//     {
-//       src: `${acadlixOptions?.pdf_url}fonts/NotoSansDevanagari-Bold.ttf`,
-//       fontWeight: "bold",
-//     },
-//   ],
-// });
+Font.register({
+  family: "NotoDevanagari",
+  src: `${acadlixOptions?.pdf_url}fonts/NotoSansDevanagari-Regular.ttf`,
+});
+
+Font.register({
+  family: "NotoThai",
+  src: `${acadlixOptions?.pdf_url}fonts/NotoSansThai-Regular.ttf`,
+});
+
+Font.register({
+  family: "NotoArabic",
+  src: `${acadlixOptions?.pdf_url}fonts/NotoSansArabic-Regular.ttf`,
+});
+
+Font.register({
+  family: "NotoJP",
+  src: `${acadlixOptions?.pdf_url}fonts/NotoSansJP-Regular.ttf`,
+});
