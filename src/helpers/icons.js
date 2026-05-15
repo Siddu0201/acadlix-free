e1966f8926348e083887885085ff6c85a9009a58
Siddu0201 <sudhanshuag02012000@gmail.com>
2026-05-15 15:11:07 +0530

    update quiz css

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -839,6 +839,18 @@ export const FaClipboardQuestion = (props) => {
   )
 }
 
+export const FaRegClock = (props) => {
+  return (
+    <svg {...props} stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"></path></svg>
+  )
+}
+
+export const FaBookOpen = (props) => {
+  return (
+    <svg {...props} stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M542.22 32.05c-54.8 3.11-163.72 14.43-230.96 55.59-4.64 2.84-7.27 7.89-7.27 13.17v363.87c0 11.55 12.63 18.85 23.28 13.49 69.18-34.82 169.23-44.32 218.7-46.92 16.89-.89 30.02-14.43 30.02-30.66V62.75c.01-17.71-15.35-31.74-33.77-30.7zM264.73 87.64C197.5 46.48 88.58 35.17 33.78 32.05 15.36 31.01 0 45.04 0 62.75V400.6c0 16.24 13.13 29.78 30.02 30.66 49.49 2.6 149.59 12.11 218.77 46.95 10.62 5.35 23.21-1.94 23.21-13.46V100.63c0-5.29-2.62-10.14-7.27-12.99z"></path></svg>
+  )
+}
+
 export const iconMap = {
   IoClose: <IoClose />,
   IoMdRefresh: <IoMdRefresh />,
@@ -961,5 +973,7 @@ export const iconMap = {
   SiGooglemeet: <SiGooglemeet />,
   BsMicrosoftTeams: <BsMicrosoftTeams />,
   KnitPayIcon: <KnitPayIcon />,
-  FaClipboardQuestion: <FaClipboardQuestion />
+  FaClipboardQuestion: <FaClipboardQuestion />,
+  FaRegClock: <FaRegClock />,
+  FaBookOpen: <FaBookOpen />
 };
