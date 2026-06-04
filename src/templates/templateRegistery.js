11f3156f543ee2db7e0bbd58e89925e8d1e3b5d2
Siddu0201 <sudhanshuag02012000@gmail.com>
2026-06-04 09:58:31 +0530

    update dependency version

--- src/templates/templateRegistery.js
+++ src/templates/templateRegistery.js
@@ -17,7 +17,7 @@ export const templateRegistry = {
     shortcode: {
       "template-1": require("./quiz/shortcode/Template1").default,
       "template-2": require("./quiz/shortcode/Template2").default,
-      "template-3": require("./quiz/shortcode/Template3").default
+      // "template-3": require("./quiz/shortcode/Template3").default
     }
   }
 };
